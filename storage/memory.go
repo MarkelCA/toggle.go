b6d1afc1c272670343ef287545e44ab1097129dc
MarkelCA <cuestaarribas.markel@proton.me>
2023-12-19 21:46:10 +0100

    Adapted memory to the new storage interface (still buggy)

--- storage/memory.go
+++ storage/memory.go
@@ -1,60 +1,55 @@
 package storage
-//
-// // REFACTOR WITHOUT THE flags dependency
-//
-// // import (
-// // 	"time"
-// //     "reflect"
-// // 	"github.com/markelca/toggles/flags"
-// // )
-// //
-// type MemoryRepository struct {}
-// //
-// func NewMemoryRepository() CacheClient {
-//     return MemoryRepository{}
-// }
-// //
-// var flagsStorage map[string]interface{} = make(map[string]interface{})
-// // var flagsStorage []interface{} = make([]interface{},0)
-// // var flagsStorage []flags.Flag = make([]flags.Flag,0)
-// //
-// func(r MemoryRepository) Keys() ([]string, error) {
-//     result := make([]string, 0)
-//     for i,_ := range flagsStorage {
-//         result = append(result, i)
-//     }
-//     return result,nil
-// }
-// //
-// func(r MemoryRepository) Get(key string) (interface{}, error) {
-//     for k,v   := range flagsStorage {
-//         if k ==  key {
-//             return v,nil
-//         }
-//     }
-//     return false,nil
-// }
-//
-// //
-// // func (r MemoryRepository) Set(key string, value interface{}, expiration time.Duration) error {
-// //     v := reflect.ValueOf(value).Bool()
-// //     for i,currentFlag := range flagsStorage {
-// //         if currentFlag.Name == key {
-// //             flagsStorage[i].Value = v
-// //             return nil
-// //         }
-// //     }
-// //     // If it doesn't find it it adds it
-// //     flagsStorage = append(flagsStorage,flags.Flag{Name:key,Value:v,})
-// //     return nil
-// // }
-// //
-// // func (r MemoryRepository) Exists(name string) (bool,error) {
-// //     for _,currentFlag := range flagsStorage {
-// //         if currentFlag.Name == name {
-// //             return true,nil
-// //         }
-// //     }
-// //     return false,nil
-// // }
-// //
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+
+type MemoryRepository struct {}
+
+func NewMemoryRepository() CacheClient {
+    return MemoryRepository{}
+}
+
+var flagsStorage map[string]string = make(map[string]string)
+
+
+func(r MemoryRepository) Keys() ([]string, error) {
+    result := make([]string, 0)
+    for i,_ := range flagsStorage {
+        result = append(result, i)
+    }
+    return result,nil
+}
+
+func(r MemoryRepository) Get(key string) (string, error) {
+    for k,v := range flagsStorage {
+        if k == key {
+            return v,nil
+        }
+    }
+    return "",Nil
+}
+
+func (r MemoryRepository) Set(key string, value interface{}, expiration time.Duration) error {
+    for k,v := range flagsStorage {
+        if k == key {
+            flagsStorage[key] = v
+            return nil
+        }
+    }
+    // If it doesn't find it it adds it
+    log.Printf("fmt: %v",value)
+    flagsStorage[key] = fmt.Sprint("%v",value)
+    return nil
+}
+
+func (r MemoryRepository) Exists(name string) (bool,error) {
+    if _, ok := flagsStorage[name]; ok {
+        return true,nil
+    }
+    return false,nil
+}
+
