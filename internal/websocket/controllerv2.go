d845a52c49467e6af4a009b6b4da5f224c267dba
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-05-25 21:57:10 +0200

    Removed debug log

--- internal/websocket/controllerv2.go
+++ internal/websocket/controllerv2.go
@@ -3,7 +3,6 @@ package websocket
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"log/slog"
 
 	"github.com/markelca/toggles/pkg/flags"
@@ -20,7 +19,6 @@ type ControllerV2 struct {
 }
 
 func (controller ControllerV2) HandleMessage(message []byte, c *Client) {
-	log.Println(message)
 	var action Action
 	c.actionMarshaller.Unmarshal(message, &action)
 	// action.Type =
